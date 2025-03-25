536862f352681a2eb2767c661338c17a4d143ae7
cpu624049 <kjs940426@naver.com>
2025-03-25 15:14:33 +0900

    250325 create SimpleBoardTest & .gitignore

---
+++ SimpleBoardTest/SimpleBoardTest/Data/ShipDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleBoardTest.Data
+{
+    public class ShipDbContext : DbContext
+    {
+        public ShipDbContext() {}
+        public ShipDbContext(DbContextOptions<ShipDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DBSet
+    }
+}
